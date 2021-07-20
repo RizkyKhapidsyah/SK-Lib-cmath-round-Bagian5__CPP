548e7fa4249fd6aa7356875d7bf7b77f1c8d0ad8
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-20 10:16:42 +0700

    Add project files.

---
+++ SK-Lib-cmath-round-Bagian5__CPP/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int x = 563;
+    double hasil;
+    hasil = round(x);
+    cout << "round(" << x << ") = " << hasil << endl;
+    _getch();
+    return 0;
+}
